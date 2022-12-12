1c344a81b9d9d66335f11ff18b6df6576c69d595
AMAN KUMAR <119098376+AMANKUMAR7562@users.noreply.github.com>
2022-12-12 21:45:23 +0530

    Update Class083-Parameters-CipherSchool.py

--- Class083-Parameters-CipherSchool.py
+++ Class083-Parameters-CipherSchool.py
@@ -2,13 +2,13 @@ def user_info(first_name,last_name,age):
     print(f"Your first name is {first_name}")
     print(f"Your last name is {last_name}")
     print(f"Your age is {age}")
-user_info('aaryan','kashyap',18)
+user_info('aman','kumar',18)
 
 def user_info(first_name,last_name='unknown',age=None):
     print(f"Your first name is {first_name}")
     print(f"Your last name is {last_name}")
     print(f"Your age is {age}")
-user_info('aaryan')
+user_info('aman')
 
 
 
